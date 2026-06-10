ec1ed2dcdf9745b73ed86bf12a98de2df9c8f4fa
Vabbajack-Sandvich <zonideedinoz2@gmail.com>
2026-06-10 05:22:04 -0400

    2026-06-10-04-19-31-AM. version 2. updating ffmpeg-gif-to-mp4-shrink-mp4-to-gif to be all in one loop. this means that it will not do the same copies of the same conversion files. it also means it will easily do 1 copy and paste of multiple gifs to vc all at once.

--- ffmpeg-gif-to-mp4-shrink-mp4-to-gif.bat
+++ ffmpeg-gif-to-mp4-shrink-mp4-to-gif.bat
@@ -1,19 +1,17 @@
 
-rem 2026-06-03-01-58-23-AM
-rem cleared the junk out of the start of the file
-
-rem 2026-06-03-02-32-02-AM
-rem updating to convert back to gif
+rem 2026-06-10-04-19-31-AM
+rem version 2
+rem just adding everything to just the one
+rem this means that it will not do the same copies of the same conversion files
+rem it also means it will easily do 1 copy and paste of multiple gifs to vc all at once
 
 cd c:\vc
 
-for %%f in (*.gif) do ffmpeg -i "%%f" -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" "%%~nf_conv.mp4"
-
-rem for %%f in (*.mp4) do ffmpeg -y -i "%%f" -c:v libx264 -b:v 500k -crf 30 -preset fast -vf scale=960:540 -r 30 "%%~nf_shrunk%%~xf"
-
-for %%f in (*.mp4) do ffmpeg -y -i "%%f" -c:v libx264 -b:v 500k -crf 30 -preset fast -vf scale=480:270 -r 30 "%%~nf_shrunk%%~xf"
-
-rem ffmpeg -i input.mp4 output.gif
-
-for %%f in (*.mp4) do ffmpeg -i "%%f" "%%~nf_conv.gif"
-
+for %%f in (*.gif) do (
+ffmpeg -i "%%f" -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" "%%~nf_conv.mp4"
+timeout /t 2 /nobreak
+ffmpeg -y -i "%%~nf_conv.mp4" -c:v libx264 -b:v 500k -crf 24 -preset fast -vf scale=290:742 -r 24 "%%~nf_shrunk.mp4"
+timeout /t 2 /nobreak
+ffmpeg -i "%%~nf_shrunk.mp4" "%%~nf_conv.gif"
+timeout /t 2 /nobreak
+)
