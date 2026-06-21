ff18e9bf053147000f868e5b9cfe76aa2bb1356d
Vabbajack-Sandvich <zonideedinoz2@gmail.com>
2026-06-21 05:13:41 -0400

    updating batch files file renaming loops

--- ffmpeg-all-files-space-to-dash.bat
+++ ffmpeg-all-files-space-to-dash.bat
@@ -4,11 +4,14 @@ rem space to dash
 cd c:\vc
 rem cd c:\vc\t
 
+rem !variable:search=replace!
+setlocal enabledelayedexpansion
+
 for %%f in ("*.*") do (
 set filename=%%f
-echo %filename%
-set filename2=%filename: =-%
-echo %filename2%
-ren "%%f" "%filename2%"
+echo !filename!
+set filename2=!filename: =-!
+echo !filename2!
+ren "%%f" "!filename2!"
 
 )
--- ffmpeg-all-files-space-to-underscore.bat
+++ ffmpeg-all-files-space-to-underscore.bat
@@ -4,11 +4,14 @@ rem space to underscore
 cd c:\vc
 rem cd c:\vc\t
 
+rem !variable:search=replace!
+setlocal enabledelayedexpansion
+
 for %%f in ("*.*") do (
 set filename=%%f
-echo %filename%
-set filename2=%filename: =_%
-echo %filename2%
-ren "%%f" "%filename2%"
+echo !filename!
+set filename2=!filename: =_!
+echo !filename2!
+ren "%%f" "!filename2!"
 
 )
