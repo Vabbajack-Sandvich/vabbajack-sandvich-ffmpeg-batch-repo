a11fc0bb2a20e88377f64805e7ff7dc06d9a5cfa
Vabbajack-Sandvich <zonideedinoz2@gmail.com>
2026-07-14 18:24:45 -0400

    updating batch files adding ffmpeg-mp3-stripmetadata-with-delete.bat

---
+++ ffmpeg-mp3-stripmetadata-with-delete.bat
@@ -0,0 +1,50 @@
+@rem 2026-07-14-13-43-15-PM
+@rem strips metadata from mp3
+@rem then deletes the file
+@rem cd C:\vc
+@rem md c:\vc\t
+@rem cd C:\zonide\non-zip\music
+@rem md C:\zonide\non-zip\music\t
+
+@rem @echo off
+setlocal EnableDelayedExpansion
+
+@rem ================================================
+@rem          change this
+@rem ================================================
+
+@rem put \ at end of path
+@rem correct:
+@rem set "zsrcpath=C:\vc\"
+@rem incorrect:
+@rem set "zsrcpath=C:\vc"
+
+@rem set "zsrcpath=C:\vc\"
+set "zsrcpath=C:\zonide\non-zip\music\"
+echo "zsrcpath - !zsrcpath!"
+@rem ================================================
+@rem ================================================
+
+set "zdestpath=!zsrcpath!t\"
+echo "zdestpath - !zdestpath!"
+
+set "zfile=zfilelist.txt"
+echo "zfile !zfile!"
+
+cd !zsrcpath!
+
+@rem make temp folder in whatever source path
+md !zdestpath!
+
+for %%f in (*.mp3) do (
+ffmpeg -y -i "%%f" -map_metadata -1 -c copy "%%~nf-md.mp3"
+timeout /t 1 /nobreak
+@rem move stripped suffixed file to temp
+echo "moving - %%~nf-md.mp3"
+move /y "%%~nf-md.mp3" "!zdestpath!"
+@rem pause
+@rem delete original mp3
+echo "deleting - %%f"
+del /f /q "%%f"
+timeout /t 1 /nobreak
+)
